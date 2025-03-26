480f8e9f92fad3a8f7918bff538b5c63144b566c
Hyppolite T. <hyppolitak@gmail.com>
2025-03-26 01:15:31 +0100

    Création et implémentation d'un controlleur pour l'authentication

---
+++ backend/app/Http/Controllers/AuthController.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    /**
+     * Register user.
+     */
+    public function register(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|string|confirmed',
+        ]);
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        return response()->json(['token' => $user->createToken('TaskManagerApp')->plainTextToken]);
+    }
+
+    /**
+     * Login.
+     */
+    public function login(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required|string',
+        ]);
+
+        if (Auth::attempt(['email' => $request->email, 'password' => $request->password])) {
+            return response()->json(['token' => Auth::user()->createToken('TaskManagerApp')->plainTextToken]);
+        }
+
+        return response()->json(['message' => 'Unauthorized'], 401);
+    }
+
+    /**
+     * Logout.
+     */
+    public function logout(Request $request)
+    {
+        $request->user()->tokens->each(function ($token) {
+            $token->delete();
+        });
+
+        return response()->json(['message' => 'Logged out successfully']);
+    }
+}
