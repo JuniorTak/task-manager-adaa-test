f32b64c30b10286e111dd05c11450ff173f16763
Hyppolite T. <hyppolitak@gmail.com>
2025-04-04 00:35:57 +0100

    Modification de la page de mise à jour de tâche pour intégrer le champ 'Type de tâche' de type radio et adaptation des appels d'API en conséquence

--- frontend/src/app/tasks/edit/[id]/page.js
+++ frontend/src/app/tasks/edit/[id]/page.js
@@ -9,7 +9,7 @@ export default function Edit() {
   const { id } = useParams(); // Get task ID from the URL.
   const router = useRouter();
   const [error, setError] = useState(null);
-  const [task, setTask] = useState({ title: "", description: "", due_date: "", user_id: null });
+  const [task, setTask] = useState({ title: "", description: "", due_date: "", user_id: null, is_private: null });
   const [token, setToken] = useState(null);
   const [initialLoading, setInitialLoading] = useState(true);
   const [loading, setLoading] = useState(false);
@@ -35,7 +35,7 @@ export default function Edit() {
       router.push("/"); // Redirect to the main page if the user ID is not the same as the task ID.
       return;
     }
-  }, [task]);
+  }, [router, task]);
 
   // Logout function.
   function handleLogout() {
@@ -55,7 +55,6 @@ export default function Edit() {
       setTask(responseData.data || []);
 
     } catch (err) {
-      console.log(err);
       // Customize error message based on error type.
       let errorMessage = "Une erreur s'est produite. Veuillez réessayer.";
       if (err.message.includes("Failed to fetch")) {
@@ -79,6 +78,7 @@ export default function Edit() {
         title: task.title,
         description: task.description,
         due_date: task.due_date,
+        is_private: task.is_private,
       };
       const res = await fetch(`${API_URL}/tasks/${id}`, {
         method: "PUT",
@@ -138,7 +138,7 @@ export default function Edit() {
               onChange={(e) => setTask({ ...task, description: e.target.value })}
               className="border p-2 rounded w-full mb-2"
             />
-            <label htmlFor="task-due-date" className="mt-1">Date d'échéance</label>
+            <label htmlFor="task-due-date" className="mt-1">Date d&apos;échéance</label>
             <input
               type="date"
               id="task-due-date"
@@ -146,6 +146,31 @@ export default function Edit() {
               onChange={(e) => setTask({ ...task, due_date: e.target.value })}
               className="border p-2 rounded w-full mb-2"
             />
+            
+            <fieldset className="mb-4">
+              <legend className="mt-1">Type de tâches</legend>
+              <label htmlFor="public-task-type" className="mr-4">
+                Publique
+                <input
+                  type="radio"
+                  id="public-task-type"
+                  name="task-type"
+                  checked={task.is_private !== null && !task.is_private}
+                  onChange={(e) => setTask({ ...task, is_private: 0 })}
+                  className="ml-2"
+                />
+              </label>
+              <label htmlFor="private-task-type" className="">
+                Privée
+              <input
+                type="radio"
+                id="private-task-type"
+                name="task-type"
+                checked={!!task.is_private} // Double negation since is_private can be null, 0 or 1.
+                onChange={(e) => setTask({ ...task, is_private: 1 })}
+                className="ml-2"
+              /></label>
+            </fieldset>
             
             <div className="space-x-2">
               <button onClick={() => updateTask(token)} className="bg-blue-500 text-white px-4 py-1 mb-1 rounded" disabled={loading}>
