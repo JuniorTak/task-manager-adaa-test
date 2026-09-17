ceeff0e59bb5e6726c1da4f360e493ccff5d4b1c
Hyppolite T. <hyppolitak@gmail.com>
2026-09-17 17:13:52 +0100

    fix(frontend): résoudre l'ensemble des erreurs d'hydratation pour éviter les conflits d'extensions

--- frontend/src/app/layout.js
+++ frontend/src/app/layout.js
@@ -18,10 +18,11 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+        suppressHydrationWarning
+        className={`${geistSans.  variable} ${geistMono.variable} antialiased`}
+      >     
         {children}
       </body>
     </html>
